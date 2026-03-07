a085eda28e26730814791d8296b20a013eaa55fc
yuzuyua <rjbc226@gmail.com>
2026-03-07 15:27:17 +0900

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -43,7 +43,6 @@ client.once("ready", async () => {
   console.log(`${client.user.tag} がログインしました！`);
 });
 
-console.log("TOKEN:", process.env.TOKEN);
 client.login(process.env.TOKEN);
 
 client.on("interactionCreate", async (interaction) => {
